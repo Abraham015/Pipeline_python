75e0a7629769ba431d26f26a2a2e78cd761b9ab1
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2025-01-07 11:07:39 -0600

    WIP

--- main.py
+++ main.py
@@ -24,8 +24,10 @@ def run_pipeline(custom_args, beam_args):
         palabras=lineas | beam.FlatMap(lambda l: l.split())
         #Esta función nos va a regresar una tupla con el contador y la palabra
         contadas: PCollection[Tuple[str, int]]= palabras | beam.combiners.Count.PerElement()
-        palabras_top=contadas | beam.combiners.Top.Of(5, key=lambda kv:kv[1])
-        palabras_top | beam.Map(print)
+        palabras_top_lista=contadas | beam.combiners.Top.Of(5, key=lambda kv:kv[1])
+        palabras_top=palabras_top_lista | beam.FlatMap(lambda x: x)
+        formateado=palabras_top | beam.Map(lambda kv: "%s, %d" % (kv[0], kv[1]))
+        formateado | beam.Map(print)
 
 if __name__=='__main__':
     main()
